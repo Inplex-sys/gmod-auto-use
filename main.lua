1b570fcf7fe95aaf86aed59d7df0096b0e7641ea
Inplex-sys <69421356+Inplex-sys@users.noreply.github.com>
2022-06-14 15:21:12 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,49 @@
+surface.CreateFont("Staff_Font", {
+    font = "Arial",
+    extended = false,
+    size = 16,
+    weight = 500,
+    blursize = 0,
+    scanlines = 0,
+    antialias = true,
+    underline = false,
+    italic = true,
+    strikeout = false,
+    symbol = false,
+    rotary = false,
+    shadow = true,
+    additive = false,
+    outline = false,
+})
+
+_G['autoUse'] = false
+
+hook.Add("CreateMove", "auto_use", function(cl_cmd)
+    if input.IsKeyDown(KEY_J) then
+        if _G['autoUse'] == true then
+            _G['autoUse'] = false
+        else
+            _G['autoUse'] = true
+        end
+    end
+end)
+
+hook.Add("HUDPaint", "DrawAutoUse", function()
+    status = ""
+    if _G['autoUse'] == true then
+        status = "enabled"
+    else
+        status = "disabled"
+    end
+
+    draw.DrawText("The Auto-Use is " .. status, "Staff_Font", ScrW() - ScrW() + 200, ScrH() - ScrH() + 24, Color(255, 255, 255, 255), 1)
+end)
+
+timer.Create("auto_use_timer", .5, 0, function()
+    if _G['autoUse'] == true then
+        RunConsoleCommand("+use")
+        timer.Simple(.5, function()
+            RunConsoleCommand("-use")
+        end)
+    end
+end)
